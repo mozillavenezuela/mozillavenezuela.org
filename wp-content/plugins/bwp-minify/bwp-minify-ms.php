86a5d751d1d4bcde88c58e41f0a558606aa16044
humberts90 <humberts90@gmail.com>
2015-09-19 13:17:31 -0430

    actualizando

--- wp-content/plugins/bwp-minify/bwp-minify-ms.php
+++
@@ -1,2 +0,0 @@
-<?php
-include_once dirname(__FILE__) . '/bwp-minify/bwp-minify.php';
--- wp-content/plugins/wp-maintenance-mode/views/notice.php
+++
@@ -1,5 +0,0 @@
-<?php foreach ($notices as $notice) { ?>
-    <div id="message" class="<?php echo $notice['class']; ?> fade">
-        <p><?php echo $notice['msg']; ?></p>
-    </div>
-<?php } ?>
